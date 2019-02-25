de60bdc9e0623fac2e394799888bc2bded22bb59
James-Chun <jchun00@stuy.edu>
2019-02-25 14:12:51 -0500

    tested using Mr. K's tests

--- QueenBoard.java
+++ QueenBoard.java
@@ -168,6 +168,34 @@ public class QueenBoard{
   }
 
 
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//TESTING
+
+//testcase must be a valid index of your input/output array
+public static void runTest(int i){
+  QueenBoard b;
+  int[]tests =   {1,2,3,4,5,8};
+  int[]answers = {1,0,0,2,10,92};
+  if(i >= 0 && i < tests.length ){
+    int size = tests[i];
+    int correct = answers[i];
+    b = new QueenBoard(size);
+    int ans  = b.countSolutions();
+
+    if(correct==ans){
+      System.out.println("PASS board size: "+tests[i]+" "+ans);
+    }else{
+      System.out.println("FAIL board size: "+tests[i]+" "+ans+" vs "+correct);
+    }
+  }
+}
+
+
+
+
   public static void main(String[] args){
     //QueenBoard chess = new QueenBoard(Integer.parseInt(args[0]));
     /*
@@ -185,7 +213,15 @@ public class QueenBoard{
     //System.out.println(chess);
   //  System.out.println(chess.toStringDebug());
   //  System.out.println(chess.countSolutions());
-
+/*
+    runTest(0);
+    runTest(1);
+    runTest(2);
+    runTest(3);
+    runTest(4);
+    runTest(5);
+    runTest(6);
+    */
   }
 
 
